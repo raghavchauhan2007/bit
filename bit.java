d1a798eb94962c83b4501855ad80885d146bacc5
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-09 20:33:59 +0530

    updateBit

--- bit.java
+++ bit.java
@@ -1,10 +1,22 @@
+import java.util.*;
 public class bit {
 
     public static void main(String[] args) {
-        int n = 5;
-        int pos = 3;
-        int bitMask = 1<<pos;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int pos = sc.nextInt();
+        int bit = sc.nextInt();
 
-        System.out.println(bitMask | n);
+        if(bit == 0){
+            int bitMask = ~(1<<pos);
+            System.out.println(bitMask & n);
+        }
+        else if(bit == 1){
+            int bitMask = 1<<pos;
+            System.out.println(bitMask | n);
+        }
+        else{
+            System.out.println("invalid bit");
+        }
     }
 }
