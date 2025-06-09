7b686b8d71d124dd57aa36adf18e0f65458387a7
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-09 17:19:54 +0530

    set

--- bit.java
+++ bit.java
@@ -2,14 +2,9 @@ public class bit {
 
     public static void main(String[] args) {
         int n = 5;
-        int pos = 2;
+        int pos = 3;
         int bitMask = 1<<pos;
 
-        if((bitMask & n) == 0){
-            System.out.println("bit was zero");
-        }
-        else{
-            System.out.println("bit was one");
-        }
+        System.out.println(bitMask | n);
     }
 }
